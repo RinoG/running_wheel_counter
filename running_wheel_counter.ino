1ca6521385700c5787617d0de597334490c71858
RinoG <45918777+RinoG@users.noreply.github.com>
2021-02-09 15:14:14 +0100

    bugfix voltage

--- running_wheel_counter.ino
+++ running_wheel_counter.ino
@@ -3,7 +3,7 @@
  * @author RinoG
  * @brief 
  * @version 0.1
- * @date 2021-02-01
+ * @date 2021-02-09
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -19,12 +19,17 @@ const int resistor_input[5][2] = {{12, 11}, {7, 6}, {15, 16}, {25, 27}, {33, 35}
 
 unsigned long prev_time_ms[5] = {0};
 
-Queue * queue_wheel_1;
-Queue * queue_wheel_2;
-Queue * queue_wheel_3;
-Queue * queue_wheel_4;
-Queue * queue_wheel_5;
+Queue * queue_time_1;
+Queue * queue_time_2;
+Queue * queue_time_3;
+Queue * queue_time_4;
+Queue * queue_time_5;
 
+Queue * queue_voltage_1;
+Queue * queue_voltage_2;
+Queue * queue_voltage_3;
+Queue * queue_voltage_4;
+Queue * queue_voltage_5;
 
 /*****************************************************************************
  * @brief setup
@@ -32,11 +37,17 @@ Queue * queue_wheel_5;
 void setup() {
   Serial.begin(57600);
   
-  queue_wheel_1 = new Queue();
-  queue_wheel_2 = new Queue();
-  queue_wheel_3 = new Queue();
-  queue_wheel_4 = new Queue();
-  queue_wheel_5 = new Queue();
+  queue_time_1 = new Queue();
+  queue_time_2 = new Queue();
+  queue_time_3 = new Queue();
+  queue_time_4 = new Queue();
+  queue_time_5 = new Queue();
+
+  queue_voltage_1 = new Queue();
+  queue_voltage_2 = new Queue();
+  queue_voltage_3 = new Queue();
+  queue_voltage_4 = new Queue();
+  queue_voltage_5 = new Queue();
 
   for (int i = 0; i <= 4; i++) {
     /* Initialize inputs from encoder (detect motor rotation) */
@@ -64,24 +75,24 @@ void loop() {
 
   if (Serial) {
     /* write data in log-file if size of Queue has changed */
-    if(queue_wheel_1->getSize() > 0) {
-      write_log(encoder_input[0],queue_wheel_1->dequeue(), counter[0]++, queue_wheel_1->dequeue(), get_resistor(resistor_input[0]));
+    if(queue_time_1->getSize() > 0) {
+      write_log(encoder_input[0],queue_time_1->dequeue(), counter[0]++, queue_voltage_1->dequeue(), get_resistor(resistor_input[0]));
     }
   
-    if(queue_wheel_2->getSize() > 0) {
-      write_log(encoder_input[1],queue_wheel_2->dequeue(), counter[1]++, queue_wheel_2->dequeue(), get_resistor(resistor_input[1]));
+    if(queue_time_2->getSize() > 0) {
+      write_log(encoder_input[1],queue_time_2->dequeue(), counter[1]++, queue_voltage_2->dequeue(), get_resistor(resistor_input[1]));
     }
   
-    if(queue_wheel_3->getSize() > 0) {
-      write_log(encoder_input[2],queue_wheel_3->dequeue(), counter[2]++, queue_wheel_3->dequeue(), get_resistor(resistor_input[2]));
+    if(queue_time_3->getSize() > 0) {
+      write_log(encoder_input[2],queue_time_3->dequeue(), counter[2]++, queue_voltage_3->dequeue(), get_resistor(resistor_input[2]));
     }
   
-    if(queue_wheel_4->getSize() > 0) {
-      write_log(encoder_input[3],queue_wheel_4->dequeue(), counter[3]++, queue_wheel_4->dequeue(), get_resistor(resistor_input[3]));
+    if(queue_time_4->getSize() > 0) {
+      write_log(encoder_input[3],queue_time_4->dequeue(), counter[3]++, queue_voltage_4->dequeue(), get_resistor(resistor_input[3]));
     }
   
-    if(queue_wheel_5->getSize() > 0) {
-      write_log(encoder_input[4],queue_wheel_5->dequeue(), counter[4]++, queue_wheel_5->dequeue(), get_resistor(resistor_input[4]));
+    if(queue_time_5->getSize() > 0) {
+      write_log(encoder_input[4],queue_time_5->dequeue(), counter[4]++, queue_voltage_5->dequeue(), get_resistor(resistor_input[4]));
     }
   }
 }
@@ -89,11 +100,11 @@ void loop() {
 /*****************************************************************************
  * @brief wheel interrupt hanlder 1-5
  *****************************************************************************/
-void handle_wheel_1() { handle_wheel(queue_wheel_1, &prev_time_ms[0], analog_pin[0]); }
-void handle_wheel_2() { handle_wheel(queue_wheel_2, &prev_time_ms[1], analog_pin[1]); }
-void handle_wheel_3() { handle_wheel(queue_wheel_3, &prev_time_ms[2], analog_pin[2]); }
-void handle_wheel_4() { handle_wheel(queue_wheel_4, &prev_time_ms[3], analog_pin[3]); }
-void handle_wheel_5() { handle_wheel(queue_wheel_5, &prev_time_ms[4], analog_pin[4]); }
+void handle_wheel_1() { handle_wheel(queue_time_1, queue_voltage_1, &prev_time_ms[0], analog_pin[0]); }
+void handle_wheel_2() { handle_wheel(queue_time_2, queue_voltage_2, &prev_time_ms[1], analog_pin[1]); }
+void handle_wheel_3() { handle_wheel(queue_time_3, queue_voltage_3, &prev_time_ms[2], analog_pin[2]); }
+void handle_wheel_4() { handle_wheel(queue_time_4, queue_voltage_4, &prev_time_ms[3], analog_pin[3]); }
+void handle_wheel_5() { handle_wheel(queue_time_5, queue_voltage_5, &prev_time_ms[4], analog_pin[4]); }
 
 /*****************************************************************************
  * @brief get_resistor
@@ -103,10 +114,10 @@ int get_resistor(const int resistor_input[]) {
   resistor_value[0] = digitalRead(resistor_input[0]);
   resistor_value[1] = digitalRead(resistor_input[1]);
   
-  if      (resistor_value[1] == 0 && resistor_value[0] == 0) R = 1;
-  else if (resistor_value[1] == 1 && resistor_value[0] == 0) R = 10;
-  else if (resistor_value[1] == 0 && resistor_value[0] == 1) R = 100;
-  else if (resistor_value[1] == 1 && resistor_value[0] == 1) R = 1000;
+  if      (resistor_value[1] == 0 && resistor_value[0] == 0) R = 1000;
+  else if (resistor_value[1] == 1 && resistor_value[0] == 0) R = 100;
+  else if (resistor_value[1] == 0 && resistor_value[0] == 1) R = 10;
+  else if (resistor_value[1] == 1 && resistor_value[0] == 1) R = 1;
   else R = 0;
 
   return R;
@@ -115,17 +126,17 @@ int get_resistor(const int resistor_input[]) {
 /*****************************************************************************
  * @brief handle_wheel
  *****************************************************************************/
-void handle_wheel(Queue * queue, unsigned long * prev_handle_time_ms, int analog_in)
+void handle_wheel(Queue * queue_time, Queue * queue_voltage, unsigned long * prev_handle_time_ms, int analog_in)
 {
   /* Debounce interrupt input, otherwise plugging in the connector will also be interpreted as a signal from the encoder. */
   unsigned long timediff_ms = millis() - (* prev_handle_time_ms);
   if (timediff_ms > 1) {
     /* write elapsed time in the Queue */
-    queue->enqueue(millis());
+    queue_time->enqueue(millis());
 
     /* write voltage in the Queue */
     unsigned long voltage = analogRead(analog_in);
-    queue->enqueue(voltage);
+    queue_voltage->enqueue(voltage);
   }
   (* prev_handle_time_ms) = millis();
 }
